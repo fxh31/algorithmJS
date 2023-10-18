fab12fcd8a68a69d80c7b5a825f2aa856e1ab4b6
dawn <1194815021@qq.com>
2023-10-18 15:27:09 +0800

    添加工具类，新增按位交换工具函数

---
+++ utils.js
@@ -0,0 +1,8 @@
+// 交换两个数的位置（arr：需要交换的数组；i，j: 需要交换的位置；）
+// 异或运算交换：相同为0，不同为1
+export const swap = (arr, i, j) => {
+  arr[i] = arr[i] ^ arr[j]
+  arr[j] = arr[i] ^ arr[j]
+  arr[i] = arr[i] ^ arr[j]
+  // return arr;
+}
