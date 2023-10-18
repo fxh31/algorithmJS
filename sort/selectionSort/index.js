0198c692d6e43832b5d90db1191489a70dbf96f2
dawn <1194815021@qq.com>
2023-10-18 10:27:51 +0800

    选择排序（selection sort）

---
+++ sort/selectionSort/index.js
@@ -0,0 +1,19 @@
+// 给定一个数组，使用选则排序的方式由小到大排列数组里的值
+function selectionSort(arr) {
+  if (arr.length < 2 || arr === null) return;
+  for (let i = 0; i < arr.length - 1; i++) { // 第一重循环：
+    let minIndex = i; // 取当前数组的第一个值作为最小值进行比较
+    for (let j = i + 1; j < arr.length - 1; j++) { // 由于选择排序每次都会确定一个值的位置，所以下一个需要比较的值只需要从 i+1 ~ length-1 即可
+      if (arr[j] < arr[minIndex]) { // 更新剩下的值里的最小值
+        minIndex = j;
+      }
+      if (minIndex !== i) { // 将最小值的位置与当前位置交换
+        let temp = arr[i];
+        arr[i] = arr[minIndex];
+        arr[minIndex] = temp;
+      }
+    }
+  }
+  return arr;
+}
+
